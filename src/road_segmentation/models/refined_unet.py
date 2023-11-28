316d995216b3799eb6c70504706ad7d89f17877a
Antoine Bergerault <antoine.bergerault@gmail.com>
2023-11-28 22:35:59 +0100

    Create refined_unet.py

---
+++ src/road_segmentation/models/refined_unet.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+from .unet import UNet
+
+class RefinedUnet(nn.Module):
+    def __init__(self, n=3) -> None:
+        super().__init__()
+        
+        # Main model
+        self.unet = UNet(n_channels=4, n_classes=1)
+        self.n = n
+        
+    def forward(self, x):
+        # Create initial predictions
+        predictions = torch.zeros_like(x[..., 0])
+        
+        for _ in self.n:
+            concatenated = torch.concat([x, predictions])
+            predictions = self.unet(concatenated)
+
+        return predictions
