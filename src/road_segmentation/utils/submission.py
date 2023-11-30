9a8fc43d57624de3ad7e008ef77a9074fa6559d3
Antoine Bergerault <antoine.bergerault@gmail.com>
2023-11-30 19:58:13 +0100

    create new data

--- src/road_segmentation/utils/submission.py
+++ src/road_segmentation/utils/submission.py
@@ -6,16 +6,16 @@ import torch.nn as nn
 
 NUMBER_OF_TEST = 50
 
-def make_submission(model, file_path, test_folder):
+def make_submission(model, device, img_transform, file_path, test_folder):
     for i in range(1, NUMBER_OF_TEST+1):
-        img = load_image(test_folder + "test_" + str(i) + "/" + "test_" + str(i) + ".png")
-        display(Image.open(test_folder + "test_" + str(i) + "/" + "test_" + str(i) + ".png"))
+        img = load_image(test_folder + "/test_" + str(i) + "/" + "test_" + str(i) + ".png")
+        #display(Image.open(test_folder + "test_" + str(i) + "/" + "test_" + str(i) + ".png"))
         img = img_transform(img).to(device)
         model.eval()
         output = nn.Sigmoid()(model.to(device)(img.unsqueeze(0)).cpu().detach()).numpy()
         output = img_float_to_uint8(output.squeeze())
         output_image = Image.fromarray(output)
-        display(output_image)
-        output_image.save("submission/" + str(i) + ".png")
+        #display(output_image)
+        output_image.save("submissions/temp/" + str(i) + ".png")
 
-    masks_to_submission(file_path, *["submission/" + str(i) + ".png" for i in range(1, NUMBER_OF_TEST+1)])
+    masks_to_submission(file_path, *["submissions/temp/" + str(i) + ".png" for i in range(1, NUMBER_OF_TEST+1)])
